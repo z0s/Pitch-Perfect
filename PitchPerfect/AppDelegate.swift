1025ed3d493f85ace5b1f7718aefeae2d343d4ea
Zeus <masgar001@gmail.com>
2016-09-21 16:10:54 -0700

    feat: Update AppDelegate File

--- PitchPerfect/AppDelegate.swift
+++ PitchPerfect/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.backgroundColor = UIColor.white
+        
+        let vc = RecordSoundsViewController()
+        let navCon = UINavigationController(rootViewController: vc)
+
+    
+        window?.rootViewController = navCon
+        window?.makeKeyAndVisible()
+        
         // Override point for customization after application launch.
         return true
     }
